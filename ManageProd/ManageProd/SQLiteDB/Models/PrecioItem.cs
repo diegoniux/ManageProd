ae08cf130333d6924c812978433f6af0e4004ec7
aridsc <aridsc@gmail.com>
2021-03-29 00:35:28 -0600

    Se anexan las clases Cliente, Direccion, Precio (pendiente de saber si se usara), Producto y Provvedor

---
+++ ManageProd/ManageProd/SQLiteDB/Models/PrecioItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ManageProd.SQLiteDB.Models
+{
+    public class PrecioItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int IdPrecio { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public decimal Descuento { get; set; }
+    }
+}
---
+++ ManageProd/ManageProd/SQLiteDB/Models/ProveedorItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ManageProd.SQLiteDB.Models
+{
+    public class ProveedorItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int IdProveedor { get; set; }
+        
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+        public string NotasProveedor { get; set; }       
+    }
+}
